827ffb51fe534b0b48ff3bc91e4e1e184712e921
tai1013-cmd <naito.taisuke999@gmail.com>
2021-12-21 14:09:12 +0900

    feat: 提案Daoを新規追加

---
+++ src/main/java/menta/app/dao/proposal/ProposalDao.java
@@ -0,0 +1,55 @@
+package menta.app.dao.proposal;
+
+import org.apache.ibatis.annotations.Param;
+
+import menta.app.dao.proposal.dataModel.ProposalDataModel;
+
+/**
+ * メンター提案マスタのDAOクラス
+ */
+public interface ProposalDao {
+
+	/**
+	 * メンター募集IDに紐付くメンター提案情報を取得する
+	 * 
+	 * @param referRecruitMentorId 参照メンター募集ID
+	 */
+	ProposalDataModel selectOneByRecruitMentorId(@Param("referRecruitMentorId") String referRecruitMentorId);
+
+	/**
+	 * ユーザーIDに紐付くメンター提案情報を取得する
+	 * 
+	 * @param referUserId 参照ユーザーID
+	 */
+	ProposalDataModel selectOneByUserId(@Param("referUserId") String referUserId);
+	
+	
+	/**
+	 * 提案IDに紐付く提案情報を取得する
+	 * 
+	 * @param proposalId 提案ID
+	 */
+	ProposalDataModel selectOneByProposalId(@Param("proposalId") String proposalId);
+	
+	/**
+	 * メンター提案テーブルに登録する
+	 * 
+	 * @param proposalDataModel 提案データモデル
+	 */
+	void insert(ProposalDataModel proposalDataModel);
+	
+	/**
+	 * メンター提案テーブルの更新する
+	 * 
+	 * @param proposalDataModel 提案データモデル
+	 */
+	void update(ProposalDataModel proposalDataModel);
+	
+	/**
+	 * メンター提案テーブルを論理削除する
+	 * 
+	 * @param proposalDataModel 提案データモデル
+	 */
+	void delete(ProposalDataModel proposalDataModel);
+	
+}
---
+++ src/main/java/menta/app/dao/proposal/dataModel/ProposalDataModel.java
@@ -0,0 +1,66 @@
+package menta.app.dao.proposal.dataModel;
+
+import java.time.LocalDateTime;
+
+import org.springframework.format.annotation.DateTimeFormat;
+import org.springframework.format.annotation.DateTimeFormat.ISO;
+
+import lombok.Getter;
+import menta.app.model.proposal.ProposalModel;
+import menta.app.model.user.valueObject.UserId;
+
+@Getter
+public class ProposalDataModel implements java.io.Serializable {
+
+	private static final long serialVersionUID = 187210220628054591L;
+
+	/**
+	 * 提案ID
+	 */
+	private String id;
+
+	/**
+	 * 内容
+	 */
+	private String detail;
+	
+	/**
+	 * 参照メンター募集ID
+	 */
+	private String referRecruitMentorId;
+		
+	/**
+	 * 参照ユーザーID
+	 */
+	private String referUserId;
+	
+	/**
+	 * レコード作成日時
+	 */
+	@DateTimeFormat(iso = ISO.DATE_TIME)
+	private LocalDateTime createdAt;
+
+	/**
+	 * レコード更新日時
+	 */
+	@DateTimeFormat(iso = ISO.DATE_TIME)
+	private LocalDateTime updatedAt;
+
+	/**
+	 * 削除フラグ
+	 */
+	private int isDeleted = 0;
+	
+	/**
+	 * コンストラクタ
+	 * @param proposalModel 提案モデル
+	 * @param referUserId 参照ユーザーID
+	 */
+	public ProposalDataModel(ProposalModel proposalModel, UserId referUserId) {
+		this.id = proposalModel.getProposalId().getId();
+		this.detail = proposalModel.getDetail();
+		this.referRecruitMentorId = proposalModel.getReferRecruitMentorId().getId();
+		this.referUserId = referUserId.getId().toString();
+	}
+	
+}
