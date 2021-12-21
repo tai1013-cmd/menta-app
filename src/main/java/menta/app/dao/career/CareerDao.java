eff93776ba145fef41d133af41e11859a0e64e47
tai1013-cmd <naito.taisuke999@gmail.com>
2021-12-21 14:08:06 +0900

    feat: 経歴Daoを新規追加

---
+++ src/main/java/menta/app/dao/career/CareerDao.java
@@ -0,0 +1,50 @@
+package menta.app.dao.career;
+
+import java.util.List;
+
+import org.apache.ibatis.annotations.Param;
+
+import menta.app.dao.career.dataModel.CareerDataModel;
+
+/**
+ * 経歴テーブルのDAOクラス
+ */
+public interface CareerDao {
+	
+	/**
+	 * 経歴テーブルからユーザIDに紐づく経歴情報一覧を取得する
+	 * 
+	 * @param referUserId 参照ユーザーID
+	 * 
+	 */
+	List<CareerDataModel> selectListByUserId(@Param("referUserId") String referUserId);
+	
+	/**
+	 * 経歴テーブルから経歴IDに紐付く経歴情報を取得する
+	 * 
+	 * @param careerId 経歴ID
+	 */
+	CareerDataModel selectOneByCareerId(@Param("careerId") String careerId);
+	
+	/**
+	 * 経歴テーブルに登録する
+	 * 
+	 * @param careerDataModel 経歴データモデル
+	 */
+	void insert(CareerDataModel careerDataModel);
+
+	/**
+	 * 経歴テーブルを更新する
+	 * 
+	 * @param careerDataModel 経歴データモデル
+	 */
+	void update(CareerDataModel careerDataModel);
+	
+	/**
+	 * 経歴テーブルを論理削除する
+	 * 
+	 * @param careerDataModel 経歴データモデル
+	 */
+	void delete(CareerDataModel careerDataModel);
+	
+}
